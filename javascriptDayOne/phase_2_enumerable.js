afe33b291aad3ff723f9203e5c608661bc2bc028
andy <morrison.andrew422@gmail.com>
2023-02-28 17:31:52 -0500

    finish myReduce

--- javascriptDayOne/phase_2_enumerable.js
+++ javascriptDayOne/phase_2_enumerable.js
@@ -1,8 +1,7 @@
-const arr = [1, 2, 3, 4]; // * 2 => [2, 4, 6, 8]
+const arr = [4, 9, 16, 25]; // * 2 => [2, 3, 4, 5]
 
 Array.prototype.myEach = function(callback) {
   for (let i = 0; i < this.length; i++) {
-    // function(callback[])
     callback(this[i]);
   }
 }
@@ -12,10 +11,30 @@ Array.prototype.myEach = function(callback) {
 
 Array.prototype.myMap = function(callback) {
   let outArr = [];
-  for (let i = 0; i < this.length; i++ ) {
-    outArr.push(callback(this[i]));
-  }
+  this.myEach(ele => outArr.push(callback(ele)));
   return outArr;
+  // for (let i = 0; i < this.length; i++ ) {
+    //   outArr.push(callback(this[i]));
+    // }
+  
+  // return outArr.push(callback(arr.myEach))
+
 }
 
-arr.myMap(ele => Math.sqrt(ele));
+// arr.myMap(ele => console.log(Math.sqrt(ele)));
+
+Array.prototype.myReduce = function(callback, initVal) {
+  let arr = this;
+
+  if (initVal === undefined) {
+    initVal = arr[0];
+    arr = arr.slice[1];
+  }
+  let result = initVal;
+  arr.myEach(ele => result = callback(result, ele));
+  return result;
+}
+
+arr.myReduce(function(acc, el) {
+  return acc + el;
+}, 5);
