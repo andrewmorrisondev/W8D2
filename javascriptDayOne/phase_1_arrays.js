d91d1264473bf44e9b5195f9e71f2368b7778b34
andy <morrison.andrew422@gmail.com>
2023-02-28 15:14:49 -0500

    twoSum

--- javascriptDayOne/phase_1_arrays.js
+++ javascriptDayOne/phase_1_arrays.js
@@ -1,7 +1,7 @@
 const array = [1, 2, 2, 3, 3, 3]
 
 Array.prototype.unique = function() {
-    let outArr = []
+    let outArr = [];
     this.forEach((ele) => {
         if (!outArr.includes(ele)) {
             outArr.push(ele);
@@ -11,3 +11,20 @@ Array.prototype.unique = function() {
 }
 
 array.unique();
+
+const twoSumArr = [-2, 2, 1, 3, -5, 5] // [[0, 1], [4, 5]]
+
+Array.prototype.twoSum = function() {
+  let pairs = [];
+  this.forEach((ele1, i1) => {
+    this.forEach((ele2, i2) => {
+      if (i2 > i1 && ele1 + ele2 === 0) {
+        pairs.push([i1, i2]);
+      }
+    })
+  })
+  return pairs;
+}
+
+console.log(twoSumArr.twoSum());
+
